dd6b80437808402496b45c94b56ffc91d1eb87e3
RRFayad <renan.r.fayad@gmail.com>
2024-12-30 11:26:52 -0300

    style: Adjust sidebar margin for lg screen

--- src/components/Chart/Sidebar/Sidebar.tsx
+++ src/components/Chart/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarChartLegend from "./SidebarChartLegend";
 
 function Sidebar() {
   return (
-    <div className="-mt-4 flex w-full gap-y-[16px] text-[12px] leading-4 md:flex-row md:items-start md:justify-center lg:flex-col lg:items-center lg:justify-center">
+    <div className="-mt-[14px] flex w-full gap-y-[16px] text-[12px] leading-4 md:flex-row md:items-start md:justify-center lg:mr-20 lg:flex-col lg:items-center lg:justify-center">
       <SidebarOptions />
       <SidebarChartLegend />
     </div>
